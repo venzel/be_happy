4933da276646b5acf3724f5267a26ccaff0e567c
Venzel (desktop) <venzelsoft@gmail.com>
2020-12-10 14:36:43 -0300

    refactor(model iuser): refatoração do modelo do usuário

---
+++ src/modules/user/shared/repositories/default/IUserRepository.ts
@@ -0,0 +1,9 @@
+interface IUserRepository {
+    create(): void
+    update(): void
+    delete(): void
+    show(): void
+    list(): void
+}
+
+export { IUserRepository }
---
+++ src/modules/user/shared/repositories/default/UserRepository.ts
@@ -0,0 +1,15 @@
+import { IUserRepository } from './IUserRepository'
+
+class UserRepository implements IUserRepository {
+    public create(): void {}
+
+    public update(): void {}
+
+    public delete(): void {}
+
+    public show(): void {}
+
+    public list(): void {}
+}
+
+export { UserRepository }
