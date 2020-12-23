36f8165ccf67c1ac6f336311ad4e43c936ca1a66
Venzel (notebook) <venzelsoft@gmail.com>
2020-12-23 13:26:06 -0300

    refactor(app): criado a configuracao estatica de files e inserido o morgan

--- src/shared/infra/http/Middleware.ts
+++ src/shared/infra/http/Middleware.ts
@@ -2,11 +2,13 @@ import express, { Express } from 'express'
 import { route } from './Route'
 import cors from 'cors'
 import helmet from 'helmet'
+import morgan from 'morgan'
 import 'express-async-errors'
 import '@shared/containers'
 import * as Sentry from '@sentry/node'
 import { sentry_dsn } from '@configs/sentry'
 import { environment } from '@configs/geral'
+import { avatars_folder } from '@configs/storage'
 import { exception } from './Exception'
 
 class Middleware {
@@ -21,7 +23,11 @@ class Middleware {
 
         app.use(helmet())
 
+        app.use(morgan('dev'))
+
         app.use(express.urlencoded({ extended: true }))
+
+        app.use('/files', express.static(avatars_folder))
 
         app.use(express.json())
 
